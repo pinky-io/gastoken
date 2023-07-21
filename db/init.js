a87a0d37db6488edb5197234d2534f94162ab47c
jhache <julien.hache7@gmail.com>
2023-07-21 21:52:53 +0200

    feat(db): First iteration ofthe init.js script.
    iterate over blocks and push their data on db every 50 blocks.

--- db/init.js
+++ db/init.js
@@ -39,27 +39,20 @@ function getBlockNbSteps(firstBlockNb, lastBlockNb, steps) {
 }
 
 const FIRST_BLOCK = 3930684;
+const LAST_BLOCK = 3937884;
+const STEPS_NB = 50;
 const rpc = "https://eth-sepolia.public.blastapi.io";
 
 async function pushToDB() {
     const provider = new ethers.JsonRpcProvider(rpc);
     const blockNb = await provider.getBlockNumber();
 
-    const steps = [FIRST_BLOCK, FIRST_BLOCK + 50] // Veuillez modifier cela en fonction de vos besoins
+    const steps = getBlockNbSteps(FIRST_BLOCK, LAST_BLOCK, STEPS_NB);
 
     for (let i = 0; i < steps.length - 1; ++i) {
         const data = await getXBlockData(provider, steps[i], steps[i + 1]);
         console.log(`pushing blocks ${steps[i]} to ${steps[i + 1]}...`);
-        const res = await addBlocksToDB(data);
-        console.log("🚀 ~ file: init.js:57 ~ pushToDB ~ res:", res)
-
-        
+        const res = addBlocksToDB(data);
+        console.log("🚀 ~ file: init.js:57 ~ pushToDB ~ res:", await res)
     }
 }
-
-
-pushToDB()
-
-
-//provider.getBlockNumber().then(blockNumber => getBlockData(provider, blockNumber)).then(console.log);
-
