7c019cd5b478d6d3823a348980b737c1f8109ec2
jhache <julien.hache7@gmail.com>
2023-07-23 00:14:27 +0200

    feat(protocol): Update PriceFeed.sol with actual oracle format.

--- gastoken-protocol/src/PriceFeed.sol
+++ gastoken-protocol/src/PriceFeed.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.19;
 
 import "./Interfaces/IPriceFeed.sol";
-import "./Dependencies/AggregatorV3Interface.sol";
+import "./Interfaces/IAutomatedFunctionsConsumer.sol";
 import "./Dependencies/SafeMath.sol";
 import "./Dependencies/Ownable.sol";
 import "./Dependencies/CheckContract.sol";
@@ -21,48 +21,27 @@ contract PriceFeed is Ownable, CheckContract, BaseMath, IPriceFeed {
 
     string public constant NAME = "PriceFeed";
 
-    AggregatorV3Interface public priceAggregator; // Mainnet Chainlink aggregator
+    IAutomatedFunctionsConsumer public priceAggregator; // from Chainlink
 
     // Core Liquity contracts
     address borrowerOperationsAddress;
     address troveManagerAddress;
 
-    // Use to convert a price answer to an 18-digit precision uint
-    uint256 public constant TARGET_DIGITS = 18;
-
     // Maximum time period allowed since Chainlink's latest round data timestamp, beyond which Chainlink is considered frozen.
     uint256 public constant TIMEOUT = 14400; // 4 hours: 60 * 60 * 4
 
-    // Maximum deviation allowed between two consecutive Chainlink oracle prices. 18-digit precision.
-    uint256 public constant MAX_PRICE_DEVIATION_FROM_PREVIOUS_ROUND = 5e17; // 50%
-
     // The last good price seen from an oracle by Liquity
     uint256 public lastGoodPrice;
-
-    struct ChainlinkResponse {
-        uint80 roundId;
-        int256 answer;
-        uint256 timestamp;
-        bool success;
-        uint8 decimals;
-    }
 
     // --- Dependency setters ---
 
     function setAddresses(address _priceAggregatorAddress) external onlyOwner {
         checkContract(_priceAggregatorAddress);
 
-        priceAggregator = AggregatorV3Interface(_priceAggregatorAddress);
+        priceAggregator = IAutomatedFunctionsConsumer(_priceAggregatorAddress);
 
         // Get an initial price from Chainlink to serve as first reference for lastGoodPrice
-        ChainlinkResponse memory chainlinkResponse = _getCurrentChainlinkResponse();
-        ChainlinkResponse memory prevChainlinkResponse =
-            _getPrevChainlinkResponse(chainlinkResponse.roundId, chainlinkResponse.decimals);
-
-        require(
-            !_chainlinkIsBroken(chainlinkResponse, prevChainlinkResponse) && !_chainlinkIsFrozen(chainlinkResponse),
-            "PriceFeed: Chainlink must be working and current"
-        );
+        bytes memory chainlinkResponse = _getCurrentChainlinkResponse();
 
         _storeChainlinkPrice(chainlinkResponse);
 
@@ -81,95 +60,25 @@ contract PriceFeed is Ownable, CheckContract, BaseMath, IPriceFeed {
     *
     */
     function fetchPrice() external override returns (uint256) {
-        // Get current and previous price data from Chainlink
-        ChainlinkResponse memory chainlinkResponse = _getCurrentChainlinkResponse();
-        ChainlinkResponse memory prevChainlinkResponse =
-            _getPrevChainlinkResponse(chainlinkResponse.roundId, chainlinkResponse.decimals);
+        // Get current price data from Chainlink
+        // temporary mock data for testing purpose (should be done in test file)
+        return 20 gwei / 1000;
+        // bytes memory chainlinkResponse = _getCurrentChainlinkResponse();
 
-        if (
-            _chainlinkIsBroken(chainlinkResponse, prevChainlinkResponse) || _chainlinkIsFrozen(chainlinkResponse)
-                || _chainlinkPriceChangeAboveMax(chainlinkResponse, prevChainlinkResponse)
-        ) {
-            return lastGoodPrice;
-        } else {
-            // If Chainlink is working, return Chainlink current price
-            return _storeChainlinkPrice(chainlinkResponse);
-        }
+        // if (!_chainlinkIsBroken(chainlinkResponse)) {
+        //     return _storeChainlinkPrice(chainlinkResponse);
+        // } else {
+        //     return lastGoodPrice;
+        // }
     }
 
     // --- Helper functions ---
 
-    /* Chainlink is considered broken if its current or previous round data is in any way bad. We check the previous round
-    * for two reasons:
-    *
-    * 1) It is necessary data for the price deviation check in case 1,
-    * and
-    * 2) Chainlink is the PriceFeed's preferred primary oracle - having two consecutive valid round responses adds
-    * peace of mind when using or returning to Chainlink.
-    */
-    function _chainlinkIsBroken(ChainlinkResponse memory _currentResponse, ChainlinkResponse memory _prevResponse)
-        internal
-        view
-        returns (bool)
-    {
-        return _badChainlinkResponse(_currentResponse) || _badChainlinkResponse(_prevResponse);
-    }
-
-    function _badChainlinkResponse(ChainlinkResponse memory _response) internal view returns (bool) {
-        // Check for response call reverted
-        if (!_response.success) return true;
-        // Check for an invalid roundId that is 0
-        if (_response.roundId == 0) return true;
-        // Check for an invalid timeStamp that is 0, or in the future
-        if (_response.timestamp == 0 || _response.timestamp > block.timestamp) return true;
+    function _chainlinkIsBroken(bytes memory _currentResponse) internal pure returns (bool) {
         // Check for non-positive price
-        if (_response.answer <= 0) return true;
+        if (_currentResponse.length <= 0) return true;
 
         return false;
-    }
-
-    function _chainlinkIsFrozen(ChainlinkResponse memory _response) internal view returns (bool) {
-        return block.timestamp.sub(_response.timestamp) > TIMEOUT;
-    }
-
-    function _chainlinkPriceChangeAboveMax(
-        ChainlinkResponse memory _currentResponse,
-        ChainlinkResponse memory _prevResponse
-    ) internal pure returns (bool) {
-        uint256 currentScaledPrice =
-            _scaleChainlinkPriceByDigits(uint256(_currentResponse.answer), _currentResponse.decimals);
-        uint256 prevScaledPrice = _scaleChainlinkPriceByDigits(uint256(_prevResponse.answer), _prevResponse.decimals);
-
-        uint256 minPrice = LiquityMath._min(currentScaledPrice, prevScaledPrice);
-        uint256 maxPrice = LiquityMath._max(currentScaledPrice, prevScaledPrice);
-
-        /*
-        * Use the larger price as the denominator:
-        * - If price decreased, the percentage deviation is in relation to the the previous price.
-        * - If price increased, the percentage deviation is in relation to the current price.
-        */
-        uint256 percentDeviation = maxPrice.sub(minPrice).mul(DECIMAL_PRECISION).div(maxPrice);
-
-        // Return true if price has more than doubled, or more than halved.
-        return percentDeviation > MAX_PRICE_DEVIATION_FROM_PREVIOUS_ROUND;
-    }
-
-    function _scaleChainlinkPriceByDigits(uint256 _price, uint256 _answerDigits) internal pure returns (uint256) {
-        /*
-        * Convert the price returned by the Chainlink oracle to an 18-digit decimal for use by Liquity.
-        * At date of Liquity launch, Chainlink uses an 8-digit price, but we also handle the possibility of
-        * future changes.
-        *
-        */
-        uint256 price;
-        if (_answerDigits >= TARGET_DIGITS) {
-            // Scale the returned price value down to Liquity's target precision
-            price = _price.div(10 ** (_answerDigits - TARGET_DIGITS));
-        } else if (_answerDigits < TARGET_DIGITS) {
-            // Scale the returned price value up to Liquity's target precision
-            price = _price.mul(10 ** (TARGET_DIGITS - _answerDigits));
-        }
-        return price;
     }
 
     function _storePrice(uint256 _currentPrice) internal {
@@ -177,66 +86,18 @@ contract PriceFeed is Ownable, CheckContract, BaseMath, IPriceFeed {
         emit LastGoodPriceUpdated(_currentPrice);
     }
 
-    function _storeChainlinkPrice(ChainlinkResponse memory _chainlinkResponse) internal returns (uint256) {
-        uint256 scaledChainlinkPrice =
-            _scaleChainlinkPriceByDigits(uint256(_chainlinkResponse.answer), _chainlinkResponse.decimals);
-        _storePrice(scaledChainlinkPrice);
+    function _storeChainlinkPrice(bytes memory _chainlinkResponse) internal returns (uint256) {
+        uint256 price = uint256(bytes32(_chainlinkResponse));
+        // scaling down to have a coherent value
+        price = price / 10 ** 18;
+        _storePrice(price);
 
-        return scaledChainlinkPrice;
+        return price;
     }
 
     // --- Oracle response wrapper functions ---
 
-    function _getCurrentChainlinkResponse() internal view returns (ChainlinkResponse memory chainlinkResponse) {
-        // First, try to get current decimal precision:
-        try priceAggregator.decimals() returns (uint8 decimals) {
-            // If call to Chainlink succeeds, record the current decimal precision
-            chainlinkResponse.decimals = decimals;
-        } catch {
-            // If call to Chainlink aggregator reverts, return a zero response with success = false
-            return chainlinkResponse;
-        }
-
-        // Secondly, try to get latest price data:
-        try priceAggregator.latestRoundData() returns (
-            uint80 roundId, int256 answer, uint256, /* startedAt */ uint256 timestamp, uint80 /* answeredInRound */
-        ) {
-            // If call to Chainlink succeeds, return the response and success = true
-            chainlinkResponse.roundId = roundId;
-            chainlinkResponse.answer = answer;
-            chainlinkResponse.timestamp = timestamp;
-            chainlinkResponse.success = true;
-            return chainlinkResponse;
-        } catch {
-            // If call to Chainlink aggregator reverts, return a zero response with success = false
-            return chainlinkResponse;
-        }
-    }
-
-    function _getPrevChainlinkResponse(uint80 _currentRoundId, uint8 _currentDecimals)
-        internal
-        view
-        returns (ChainlinkResponse memory prevChainlinkResponse)
-    {
-        /*
-        * NOTE: Chainlink only offers a current decimals() value - there is no way to obtain the decimal precision used in a 
-        * previous round.  We assume the decimals used in the previous round are the same as the current round.
-        */
-
-        // Try to get the price data from the previous round:
-        try priceAggregator.getRoundData(_currentRoundId - 1) returns (
-            uint80 roundId, int256 answer, uint256, /* startedAt */ uint256 timestamp, uint80 /* answeredInRound */
-        ) {
-            // If call to Chainlink succeeds, return the response and success = true
-            prevChainlinkResponse.roundId = roundId;
-            prevChainlinkResponse.answer = answer;
-            prevChainlinkResponse.timestamp = timestamp;
-            prevChainlinkResponse.decimals = _currentDecimals;
-            prevChainlinkResponse.success = true;
-            return prevChainlinkResponse;
-        } catch {
-            // If call to Chainlink aggregator reverts, return a zero response with success = false
-            return prevChainlinkResponse;
-        }
+    function _getCurrentChainlinkResponse() internal returns (bytes memory chainlinkResponse) {
+        return priceAggregator.latestResponse();
     }
 }
